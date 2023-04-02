37808850cc283841ff6fb1502cc10f1983330671
santhoshkumar <its.santhoshkumar@outlook.com>
2023-04-02 12:42:32 +0530

    fix: changes in the design

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-outerLoop:
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
-    // if (i === 5 && j === 5) {
-    //   continue outerLoop;
-    // }
-    if (i === 5 && j === 5) {
-        break outerLoop;
-      }
-    console.log(`i = ${i}, j = ${j}`);
-  }
-}
+// // for loop with labels
+// outerLoop:
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     // if (i === 5 && j === 5) {
+//     //   continue outerLoop;
+//     // }
+//     if (i === 5 && j === 5) {
+//       break outerLoop;
+//     }
+//     // console.log(`i = ${i}, j = ${j}`);
+//   }
+// }
+
+// const cars = ["a", "Volvo", "Saab", "Ford"];
+// let text = '';
+// list: {
+//   text += cars[0] + "<br>";
+//   if (text.length < 5) {
+//     break list;
+//   }
+//   text += cars[1] + "<br>";
+//   text += cars[2] + "<br>";
+//   text += cars[3] + "<br>";
+// }
+// console.log(text);
